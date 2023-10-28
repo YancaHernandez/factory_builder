6dd0b60f53136faa632b950fa4521da8d5badb3d
Yanca <yanher_12@hotmail.com>
2023-10-27 19:42:38 -0500

    feat: logistica_con_patron_version_3

--- src/app/infrastructure/logistica/factory/logistica_factory.ts
+++ src/app/infrastructure/logistica/factory/logistica_factory.ts
@@ -1,7 +1,8 @@
 import { EnvioModel, TipoTransporte } from "@domain/models/envio.model";
 import { Logistica_Transporte } from "./logistica_transporte";
 import { Logistica_Aerea } from "./logistica_aerea";
-import { Logistica_Carretera } from "./Logistica_Carretera";
+import { Logistica_Carretera } from "./logistica_carretera";
+import { Logistica_Maritima } from "./logistica_maritima";
 
 export class LogisticaFactory {
   static createLogistica(envio: EnvioModel): Logistica_Transporte {
@@ -10,6 +11,8 @@ export class LogisticaFactory {
         return new Logistica_Carretera(envio);
       case TipoTransporte.Aereo:
         return new Logistica_Aerea(envio);
+      case TipoTransporte.Maritimo:
+        return new Logistica_Maritima(envio);
     }
   }
 }
---
+++ src/app/infrastructure/logistica/factory/logistica_maritima.ts
@@ -0,0 +1,31 @@
+import { Logistica_Transporte } from "./logistica_transporte";
+import { EnvioModel } from "@domain/models/envio.model";
+import { direccion_centrar_por_ciudad, direccion_puerto_por_ciudad } from "@domain/data/direcciones";
+
+export class Logistica_Maritima extends Logistica_Transporte {
+  constructor(
+    envio: EnvioModel
+  ) { super(envio) }
+
+  //Calcular costo total
+  calcular_cosoto_total(): number {
+    //Calcular cargo por recorrido urbano con carga al puerto
+    this.recorrido_urbano(this.envio.direccion_destino, direccion_puerto_por_ciudad[this.envio.ciudad_origen], true);
+    //Calcular cargo por recorrido en barco
+    this.recorrido_maritimo(this.envio.ciudad_origen, this.envio.ciudad_destino);
+    //Calcular cargo por recorrido urbano con carga del puerto a la central
+    this.recorrido_urbano(direccion_puerto_por_ciudad[this.envio.ciudad_destino], direccion_centrar_por_ciudad[this.envio.ciudad_destino], true);
+
+    //Retornar valor total
+    return this.total;
+  }
+
+  private recorrido_maritimo(origen: string, destino: string) {
+    this.total += 500;
+    console.info('PATRON')
+    console.log(`Transportar por mar desde ${origen} hasta ${destino}`);
+    console.log(`Cargo por recorrido en barco: 500`);
+  }
+
+
+}
